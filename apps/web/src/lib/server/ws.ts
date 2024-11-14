423b57115316235e7b5b46484b3262175d5fcf7c
davilivob <david.iii.chiu@gmail.com>
2024-11-14 14:19:06 +0800

    [web] declare ws types

--- apps/web/src/lib/server/ws.ts
+++ apps/web/src/lib/server/ws.ts
@@ -1,9 +1,28 @@
+import type { ParseEnum } from '@repo/lib/types';
+
 import { BunWS, startHelperServer } from '@repo/lib/server';
+import { SuppliesTypeOptions } from '@repo/lib/pb';
+
 import { WS_HELPER_PORT } from '$env/static/private';
 
 type WSData = {
 	message?: string;
-	data?: any;
+	data?:
+		| {
+				type: 'supply';
+				supply_type: ParseEnum<SuppliesTypeOptions>;
+				id: string;
+				directory: string;
+		  }
+		| {
+				type: 'launch';
+				supply_amount: number;
+		  }
+		| {
+				type: 'weather';
+				raining: boolean;
+		  }
+		| {};
 };
 
 const ws = new BunWS<WSData>({

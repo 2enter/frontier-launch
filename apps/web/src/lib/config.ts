ed17ca83feac6a6ed116271a9bd28ede46c8adb4
davilivob <david.iii.chiu@gmail.com>
2024-11-15 16:30:14 +0800

    [web] declare colors for painting page

---
+++ apps/web/src/lib/config.ts
@@ -0,0 +1,19 @@
+const COLORS = [
+	{ name: 'red', value: '#EB0018' },
+	{ name: 'orange', value: '#ED6A19' },
+	{ name: 'yellow', value: '#DFDD2C' },
+	{ name: 'lime', value: '#84D044' },
+	{ name: 'green', value: '#2E8250' },
+	{ name: 'blue', value: '#3E67D6' },
+	{ name: 'cyan', value: '#53C6D9' },
+	{ name: 'purple', value: '#855AA8' },
+	{ name: 'pink', value: '#EB7DD1' },
+	{ name: 'peach', value: '#EDBEAA' },
+	{ name: 'brown', value: '#723D1E' },
+	{ name: 'black', value: '#262626' }
+] as const;
+
+type ColorName = (typeof COLORS)[number]['name'];
+
+export { COLORS };
+export type { ColorName };

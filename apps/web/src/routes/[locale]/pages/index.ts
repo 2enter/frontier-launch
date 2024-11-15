f8ffd00929ef2a865eae6caef14c4d102836cc54
davilivob <david.iii.chiu@gmail.com>
2024-11-15 16:28:29 +0800

    [web] init pages

--- apps/web/src/routes/[locale]/pages/index.ts
+++ apps/web/src/routes/[locale]/pages/index.ts
@@ -1,5 +1,8 @@
 import { default as Intro } from './intro.svelte';
-import { default as Name } from './name.svelte';
-import { default as Message } from './message.svelte';
+import { default as Description } from './description.svelte';
+import { default as Select } from './select.svelte';
+import { default as Paint } from './paint.svelte';
+import { default as Upload } from './upload.svelte';
+import { default as Result } from './result.svelte';
 
-export const Pages = [Intro, Name, Message];
+export const Pages = [Intro, Description, Select, Paint, Upload, Result];
--- apps/web/src/routes/[locale]/pages/name.svelte
+++
@@ -1,5 +0,0 @@
-<script lang="ts">
-	import { inputState } from '@/states';
-</script>
-
-<input type="text" bind:value={inputState.name} />

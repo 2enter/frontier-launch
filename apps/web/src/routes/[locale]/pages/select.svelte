de03090d57c3af738022c54e1732db6c0b822cf1
davilivob <david.iii.chiu@gmail.com>
2024-11-16 20:24:05 +0800

    [web] init select page

---
+++ apps/web/src/routes/[locale]/pages/select.svelte
@@ -0,0 +1,17 @@
+<script lang="ts">
+	import { inputState, sysState } from '@/states';
+	import { SuppliesTypeOptions } from '@repo/lib/pb';
+
+	const SUPPLY_TYPES = Object.values(SuppliesTypeOptions);
+
+	function chooseSupplyType(type: SuppliesTypeOptions) {
+		inputState.supplyType = type;
+		sysState.navigate(1);
+	}
+</script>
+
+<div class="center-content w-1/2 flex-wrap gap-3">
+	{#each SUPPLY_TYPES as type}
+		<button aria-label="button" class="size-32 bg-black" onclick={() => chooseSupplyType(type)}></button>
+	{/each}
+</div>

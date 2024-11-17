a12a5b2e60921f9630867697ed819e53849cd606
davilivob <david.iii.chiu@gmail.com>
2024-11-17 17:00:55 +0800

    [web] add new props to <ImgBtn>: ontouchstart, ontouchend

--- apps/web/src/lib/components/img_btn.svelte
+++ apps/web/src/lib/components/img_btn.svelte
@@ -1,43 +1,33 @@
 <script lang="ts">
-	import { onMount } from 'svelte';
-
 	interface Props {
 		src: string;
 		onclick?: () => void;
+		ontouchstart?: () => void;
+		ontouchend?: () => void;
 		class?: string;
 	}
 
-	let { class: className, src, onclick }: Props = $props();
+	let { class: className, src, onclick = () => {}, ontouchstart, ontouchend }: Props = $props();
 
-	let btn = $state<HTMLButtonElement>();
 	let img = $state<HTMLImageElement>();
 
-	onMount(() => {
-		if (!btn) return;
-		btn.addEventListener('touchstart', (e) => {
-			if (!img) return;
-			img.style.transform = 'scale(0.9)';
-		});
-
-		btn.addEventListener('touchend', (e) => {
-			if (!img) return;
-			img.style.transform = 'scale(1)';
-		});
-
-		return () => {
-			if (!btn) return;
-			btn.removeEventListener('touchstart', (e) => {
-				if (!img) return;
-				img.style.transform = 'scale(0.9)';
-			});
-			btn.removeEventListener('touchend', (e) => {
-				if (!img) return;
-				img.style.transform = 'scale(1)';
-			});
-		};
-	});
+	function scaleImg(num: number) {
+		if (!img) return;
+		img.style.transform = `scale(${num})`;
+	}
 </script>
 
-<button bind:this={btn} class="{className} " {onclick}>
+<button
+	class="{className} "
+	{onclick}
+	ontouchstart={() => {
+		scaleImg(0.9);
+		ontouchstart?.();
+	}}
+	ontouchend={() => {
+		scaleImg(1);
+		ontouchend?.();
+	}}
+>
 	<img bind:this={img} {src} alt="" class="transition-transform duration-100" />
 </button>

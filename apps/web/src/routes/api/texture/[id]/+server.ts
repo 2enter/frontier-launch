7f50fec6e6de10ad9336f8fdea4a284cc1b6fa59
davilivob <david.iii.chiu@gmail.com>
2024-11-17 22:27:55 +0800

    [web] GET /api/texture/[id]

---
+++ apps/web/src/routes/api/texture/[id]/+server.ts
@@ -0,0 +1,9 @@
+import { PBFile } from '@repo/lib/pb';
+import { pb } from '@/server/pb';
+
+export const GET = async ({ params }) => {
+	const { id } = params;
+	const file = new PBFile({ pb, collection: 'cargoes', field: 'texture', id });
+	const buffer = await file.getBuffer();
+	return new Response(buffer);
+};

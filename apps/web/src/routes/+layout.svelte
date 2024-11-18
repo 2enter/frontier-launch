4d5ab386bda38f0452c2ee8112e9c9901f224b4e
davilivob <david.iii.chiu@gmail.com>
2024-11-18 17:55:34 +0800

    [web] separate +layout.svelte into 2 layers

--- apps/web/src/routes/+layout.svelte
+++ apps/web/src/routes/+layout.svelte
@@ -2,38 +2,9 @@
 	import '@repo/config/app.css';
 	import '../app.css';
 
-	import { parseTime, toFixedDigit } from '@repo/lib/utils/calc';
-
-	import { onMount } from 'svelte';
-	import { fade } from 'svelte/transition';
-
 	import { PUBLIC_FA_KIT_URL } from '$env/static/public';
-	import { navigating } from '$app/stores';
-	import { dev } from '$app/environment';
-
-	import { sysState } from '@/states';
-	import { LAUNCH_TIMEOUT } from '@/config';
-	import { FullscreenChecker } from '@/components/index.js';
 
 	let { children } = $props();
-	let launchCountDown = $state<number>();
-
-	onMount(async () => {
-		if (dev) {
-			const eruda = (await import('eruda')).default;
-			eruda.init();
-		}
-
-		const interval = setInterval(async () => {
-			launchCountDown = LAUNCH_TIMEOUT - (await fetch('/api/console/duration').then((data) => data.json().then((result) => result)));
-		}, 1000);
-
-		return {
-			destroy() {
-				clearInterval(interval);
-			}
-		};
-	});
 </script>
 
 <svelte:head>
@@ -42,41 +13,4 @@
 	{/if}
 </svelte:head>
 
-<div class="fixed top-0 z-[1000] w-full text-center">
-	{#if launchCountDown}
-		{@const { minute, second } = parseTime(launchCountDown)}
-		{toFixedDigit(minute)}:{toFixedDigit(second)}
-	{/if}
-</div>
-
-<div class="center-content fixed bottom-0 z-[1000] w-full">
-	<img src="/ui/texts/2enter.png" class="h-10" alt="" />
-</div>
-
-<div class="full-screen center-content">
-	{@render children()}
-</div>
-
-<FullscreenChecker />
-
-<dialog bind:this={sysState.dialog} class="modal modal-middle">
-	<div class="modal-box">
-		<h1>{sysState.localization.error}</h1>
-		<p>{sysState.errorMessage}</p>
-		<div class="modal-action">
-			<form method="dialog">
-				<button class="btn btn-secondary" onclick={sysState.closeError}>
-					{sysState.localization.close}
-				</button>
-			</form>
-		</div>
-	</div>
-</dialog>
-
-{#if sysState.processing || $navigating}
-	<div transition:fade class="full-screen center-content z-[100000] bg-black/10 backdrop-blur-sm">
-		<i class="fa-solid fa-loader fa-duotone animate-spin text-8xl"></i>
-	</div>
-{/if}
-
-<button class="btn btn-secondary fixed left-0 top-0 z-[3000]" onclick={() => window.location.reload()}>reload</button>
+{@render children()}

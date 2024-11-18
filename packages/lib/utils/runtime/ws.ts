38307a7e1585c2b17c3bf24d20cea305fb58d654
davilivob <david.iii.chiu@gmail.com>
2024-11-18 18:08:57 +0800

    [@repo/lib] utils/runtime/ws: auto parse incoming ws message

--- packages/lib/utils/runtime/ws.ts
+++ packages/lib/utils/runtime/ws.ts
@@ -1,13 +1,22 @@
 import { sleep } from './sleep';
+import * as validator from 'validator';
 
-function makeWSClient<T>(args: { url: string; onmessage?: (e: MessageEvent<T>) => Promise<void>; onerror?: (e: Event) => Promise<void> }) {
+function makeWSClient<T>(args: { url: string; onmessage?: (data: T) => Promise<void>; onerror?: (e: Event) => Promise<void> }) {
 	const { url, onmessage, onerror } = args;
 	let ws = new WebSocket(url);
 
 	ws.onopen = () => {
 		console.log('ws connected');
 	};
-	if (onmessage) ws.onmessage = onmessage;
+	if (onmessage) {
+		ws.onmessage = async (e) => {
+			if (validator.isJSON(e.data)) {
+				await onmessage(JSON.parse(e.data));
+			} else {
+				console.log(`receiving not JSON message: ${e.data}`);
+			}
+		};
+	}
 	if (onerror) ws.onerror = onerror;
 
 	ws.onclose = async () => {

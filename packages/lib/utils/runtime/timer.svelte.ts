a60669d215ad7ee6a436701196ec9341a8e4961b
davilivob <david.iii.chiu@gmail.com>
2024-11-20 13:17:27 +0800

    [web] export Timer in 2 classes: TimerState & Timer, for svelte project & non-svelte project

--- packages/lib/utils/runtime/timer.svelte.ts
+++ packages/lib/utils/runtime/timer.svelte.ts
@@ -7,13 +7,13 @@ class Timer {
 	readonly interval: NodeJS.Timer;
 	readonly triggers: Trigger[];
 
-	startAt = $state(0);
-	now = $state(0);
+	startAt = 0;
+	now = 0;
 
 	constructor(args: { timeout?: number; triggers?: Trigger[] } = {}) {
 		const { timeout, triggers } = args;
 		this.triggers = triggers ?? [];
-		this.startAt = this.now = Date.now();
+		this.reset();
 
 		this.interval = setInterval(async () => {
 			this.now = Date.now();
@@ -45,4 +45,46 @@ class Timer {
 	}
 }
 
-export { Timer };
+class TimerState {
+	readonly interval: NodeJS.Timer;
+	readonly triggers: Trigger[];
+
+	startAt = $state(0);
+	now = $state(0);
+
+	constructor(args: { timeout?: number; triggers?: Trigger[] } = {}) {
+		const { timeout, triggers } = args;
+		this.triggers = triggers ?? [];
+		this.reset();
+
+		this.interval = setInterval(async () => {
+			this.now = Date.now();
+			if (!triggers) return;
+			for (const { check, action } of triggers) {
+				if (check?.() ?? true) await action();
+			}
+		}, timeout ?? 1000);
+	}
+
+	stop() {
+		clearInterval(this.interval);
+	}
+
+	reset() {
+		this.startAt = Date.now();
+	}
+
+	get duration() {
+		return this.now - this.startAt;
+	}
+
+	get info() {
+		return {
+			now: this.now,
+			duration: this.duration,
+			startAt: this.startAt
+		};
+	}
+}
+
+export { Timer, TimerState };

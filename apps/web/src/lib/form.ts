89f9e906e597e657c3d5ee5d24262015c559d1a7
davilivob <david.iii.chiu@gmail.com>
2024-11-22 16:54:07 +0800

    [web] take a function that returns files info in makeEnhanceHandler()

--- apps/web/src/lib/form.ts
+++ apps/web/src/lib/form.ts
@@ -2,13 +2,14 @@ import type { ActionResult, SubmitFunction } from '@sveltejs/kit';
 
 function makeEnhanceHandler(args: {
 	handlers: Partial<Record<ActionResult['type'], (data?: SubmitResult) => Promise<any>>>;
-	files?: { name: string; file: File }[];
+	getFiles?: () => Promise<{ name: string; file: File }[] | null>;
 	onstart?: () => void;
 	onfinish?: () => void;
 	validate?: () => any;
 	confirmMessage?: string;
 }): SubmitFunction {
-	const { handlers, files, confirmMessage, validate, onstart, onfinish } = args;
+	const { handlers, getFiles, confirmMessage, validate, onstart, onfinish } = args;
+
 	return async ({ cancel, formData }) => {
 		if (!(validate?.() ?? true) || confirmMessage ? confirm(confirmMessage) : false) {
 			cancel();
@@ -16,11 +17,14 @@ function makeEnhanceHandler(args: {
 		}
 		onstart?.();
 
+		const files = await getFiles?.();
+
 		if (files) {
 			for (const { name, file } of files) {
-				formData.append(name, file);
+				formData.append(name, file, file.name);
 			}
 		}
+
 		return async ({ update, result }) => {
 			await update({ reset: false });
 			onfinish?.();

7348af703a2921522038fcc6fb346b5aa2d8208b
davilivob <david.iii.chiu@gmail.com>
2024-11-22 16:53:13 +0800

    [web] take the same input type as sharp() in makeTextureImage()

--- apps/web/src/lib/image.ts
+++ apps/web/src/lib/image.ts
@@ -2,7 +2,21 @@ import sharp from 'sharp';
 import { RESULT_SIZE } from '@/config';
 import { CargoesTypeOptions } from '@repo/lib/pb';
 
-async function makeTextureImage(paint: Buffer | ArrayBuffer, cargoType: CargoesTypeOptions) {
+type SharpInput =
+	| Buffer
+	| ArrayBuffer
+	| Uint8Array
+	| Uint8ClampedArray
+	| Int8Array
+	| Uint16Array
+	| Int16Array
+	| Uint32Array
+	| Int32Array
+	| Float32Array
+	| Float64Array
+	| string;
+
+async function makeTextureImage(paint: SharpInput, cargoType: CargoesTypeOptions) {
 	const overlay = await sharp(`./static/cargoes/${cargoType}_canva.png`).resize(RESULT_SIZE, RESULT_SIZE).toBuffer();
 	const fixedPaint = await sharp(paint).resize(RESULT_SIZE, RESULT_SIZE, { fit: 'cover' }).toBuffer();
 

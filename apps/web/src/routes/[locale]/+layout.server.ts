5f7e925af60cb37525ccbaa9b49e7390acc8a386
davilivob <david.iii.chiu@gmail.com>
2024-12-13 17:23:57 +0800

    [web] set response headers to disable cache in /+layout.server.ts

--- apps/web/src/routes/[locale]/+layout.server.ts
+++ apps/web/src/routes/[locale]/+layout.server.ts
@@ -3,12 +3,17 @@ import type { Locale } from '@/localizations';
 import { redirect } from '@sveltejs/kit';
 import { DEFAULT_LOCALE, LOCALES, localizations } from '@/localizations';
 
-export const load = async ({ params }) => {
+export const load = async ({ setHeaders, params }) => {
 	const locale = params.locale as Locale;
 	if (!LOCALES.includes(locale)) {
 		redirect(308, `/${DEFAULT_LOCALE}`);
 	}
 	const localization = localizations[locale];
+	setHeaders({
+		'Cache-Control': 'no-cache, no-store, must-revalidate, max-age=0, s-maxage=0',
+		Pragma: 'no-cache',
+		Expires: '0'
+	});
 	return { locale, localization };
 };
 

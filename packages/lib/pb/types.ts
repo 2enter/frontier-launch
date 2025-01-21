bb7b3f1bb0954f3d7f25de11a13744c2f3269442
davilivob <david.iii.chiu@gmail.com>
2025-01-21 15:19:03 +0800

    [pb] update types

--- packages/lib/pb/types.ts
+++ packages/lib/pb/types.ts
@@ -6,7 +6,13 @@ import type PocketBase from 'pocketbase'
 import type { RecordService } from 'pocketbase'
 
 export enum Collections {
+	Authorigins = "_authOrigins",
+	Externalauths = "_externalAuths",
+	Mfas = "_mfas",
+	Otps = "_otps",
+	Superusers = "_superusers",
 	Cargoes = "cargoes",
+	CargoesArchived = "cargoes_archived",
 	News = "news",
 	Users = "users",
 }
@@ -19,8 +25,6 @@ export type HTMLString = string
 // System fields
 export type BaseSystemFields<T = never> = {
 	id: RecordIdString
-	created: IsoDateString
-	updated: IsoDateString
 	collectionId: string
 	collectionName: Collections
 	expand?: T
@@ -35,6 +39,61 @@ export type AuthSystemFields<T = never> = {
 
 // Record types for each collection
 
+export type AuthoriginsRecord = {
+	collectionRef: string
+	created?: IsoDateString
+	fingerprint: string
+	id: string
+	recordRef: string
+	updated?: IsoDateString
+}
+
+export type ExternalauthsRecord = {
+	collectionRef: string
+	created?: IsoDateString
+	id: string
+	provider: string
+	providerId: string
+	recordRef: string
+	updated?: IsoDateString
+}
+
+export type MfasRecord = {
+	collectionRef: string
+	created?: IsoDateString
+	id: string
+	method: string
+	recordRef: string
+	updated?: IsoDateString
+}
+
+export type OtpsRecord = {
+	collectionRef: string
+	created?: IsoDateString
+	id: string
+	password: string
+	recordRef: string
+	sentTo?: string
+	updated?: IsoDateString
+}
+
+export type SuperusersRecord = {
+	created?: IsoDateString
+	email: string
+	emailVisibility?: boolean
+	id: string
+	password: string
+	tokenKey: string
+	updated?: IsoDateString
+	verified?: boolean
+}
+
+export enum CargoesStatusOptions {
+	"shipping" = "shipping",
+	"shipped" = "shipped",
+	"launched" = "launched",
+}
+
 export enum CargoesTypeOptions {
 	"water" = "water",
 	"spring" = "spring",
@@ -43,45 +102,97 @@ export enum CargoesTypeOptions {
 	"cake" = "cake",
 	"diamond" = "diamond",
 }
-
-export enum CargoesStatusOptions {
-	"shipping" = "shipping",
-	"shipped" = "shipped",
-	"launched" = "launched",
-}
 export type CargoesRecord = {
+	created?: IsoDateString
 	draw_duration: number
+	id: string
 	paint: string
 	status: CargoesStatusOptions
 	texture?: string
 	type: CargoesTypeOptions
+	updated?: IsoDateString
+}
+
+export enum CargoesArchivedStatusOptions {
+	"shipping" = "shipping",
+	"shipped" = "shipped",
+	"launched" = "launched",
+}
+
+export enum CargoesArchivedTypeOptions {
+	"water" = "water",
+	"spring" = "spring",
+	"stair" = "stair",
+	"star" = "star",
+	"cake" = "cake",
+	"diamond" = "diamond",
+}
+export type CargoesArchivedRecord = {
+	created?: IsoDateString
+	draw_duration: number
+	id: string
+	paint: string
+	status: CargoesArchivedStatusOptions
+	texture?: string
+	type: CargoesArchivedTypeOptions
+	updated?: IsoDateString
 }
 
 export type NewsRecord = {
+	created?: IsoDateString
 	hype: number
+	id: string
 	title?: string
+	updated?: IsoDateString
 }
 
 export type UsersRecord = {
 	avatar?: string
+	created?: IsoDateString
+	email?: string
+	emailVisibility?: boolean
+	id: string
 	name?: string
+	password: string
+	tokenKey: string
+	updated?: IsoDateString
+	username: string
+	verified?: boolean
 }
 
 // Response types include system fields and match responses from the PocketBase API
+export type AuthoriginsResponse<Texpand = unknown> = Required<AuthoriginsRecord> & BaseSystemFields<Texpand>
+export type ExternalauthsResponse<Texpand = unknown> = Required<ExternalauthsRecord> & BaseSystemFields<Texpand>
+export type MfasResponse<Texpand = unknown> = Required<MfasRecord> & BaseSystemFields<Texpand>
+export type OtpsResponse<Texpand = unknown> = Required<OtpsRecord> & BaseSystemFields<Texpand>
+export type SuperusersResponse<Texpand = unknown> = Required<SuperusersRecord> & AuthSystemFields<Texpand>
 export type CargoesResponse<Texpand = unknown> = Required<CargoesRecord> & BaseSystemFields<Texpand>
+export type CargoesArchivedResponse<Texpand = unknown> = Required<CargoesArchivedRecord> & BaseSystemFields<Texpand>
 export type NewsResponse<Texpand = unknown> = Required<NewsRecord> & BaseSystemFields<Texpand>
 export type UsersResponse<Texpand = unknown> = Required<UsersRecord> & AuthSystemFields<Texpand>
 
 // Types containing all Records and Responses, useful for creating typing helper functions
 
 export type CollectionRecords = {
+	_authOrigins: AuthoriginsRecord
+	_externalAuths: ExternalauthsRecord
+	_mfas: MfasRecord
+	_otps: OtpsRecord
+	_superusers: SuperusersRecord
 	cargoes: CargoesRecord
+	cargoes_archived: CargoesArchivedRecord
 	news: NewsRecord
 	users: UsersRecord
 }
 
 export type CollectionResponses = {
+	_authOrigins: AuthoriginsResponse
+	_externalAuths: ExternalauthsResponse
+	_mfas: MfasResponse
+	_otps: OtpsResponse
+	_superusers: SuperusersResponse
 	cargoes: CargoesResponse
+	cargoes_archived: CargoesArchivedResponse
 	news: NewsResponse
 	users: UsersResponse
 }
@@ -90,7 +201,13 @@ export type CollectionResponses = {
 // https://github.com/pocketbase/js-sdk#specify-typescript-definitions
 
 export type TypedPocketBase = PocketBase & {
+	collection(idOrName: '_authOrigins'): RecordService<AuthoriginsResponse>
+	collection(idOrName: '_externalAuths'): RecordService<ExternalauthsResponse>
+	collection(idOrName: '_mfas'): RecordService<MfasResponse>
+	collection(idOrName: '_otps'): RecordService<OtpsResponse>
+	collection(idOrName: '_superusers'): RecordService<SuperusersResponse>
 	collection(idOrName: 'cargoes'): RecordService<CargoesResponse>
+	collection(idOrName: 'cargoes_archived'): RecordService<CargoesArchivedResponse>
 	collection(idOrName: 'news'): RecordService<NewsResponse>
 	collection(idOrName: 'users'): RecordService<UsersResponse>
 }

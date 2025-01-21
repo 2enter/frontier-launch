17499b2637ad86ea094919351a241367e4decdba
davilivob <david.iii.chiu@gmail.com>
2025-01-21 17:28:41 +0800

    [web] enable browser cache

--- apps/web/src/routes/[locale]/+layout.server.ts
+++ apps/web/src/routes/[locale]/+layout.server.ts
@@ -9,12 +9,12 @@ export const load = async ({ setHeaders, params }) => {
 		redirect(308, `/${DEFAULT_LOCALE}`);
 	}
 	const localization = localizations[locale];
-	setHeaders({
-		// 'Cache-Control': 'no-cache, no-store, must-revalidate, max-age=0, proxy-revalidate, s-maxage=0, private',
-		'Cache-Control': 'no-cache, no-store, must-revalidate, max-age=0, s-maxage=0',
-		Pragma: 'no-cache',
-		Expires: '0'
-	});
+	// setHeaders({
+	// 	// 'Cache-Control': 'no-cache, no-store, must-revalidate, max-age=0, proxy-revalidate, s-maxage=0, private',
+	// 	'Cache-Control': 'no-cache, no-store, must-revalidate, max-age=0, s-maxage=0',
+	// 	Pragma: 'no-cache',
+	// 	Expires: '0'
+	// });
 	return { locale, localization };
 };
 

0b21c9a1c8ad1e646a4ea1779d795fa6088cb14c
davilivob <david.iii.chiu@gmail.com>
2025-02-11 16:50:16 +0800

    [backend/web] prettify code

--- backend/web/src/webdriver.rs
+++ backend/web/src/webdriver.rs
@@ -3,5 +3,5 @@ use thirtyfour::prelude::*;
 pub async fn get_webdriver(port: u16) -> Result<WebDriver, WebDriverError> {
     let mut caps = DesiredCapabilities::chrome();
     caps.add_arg("--headless")?;
-    WebDriver::new(format!("http://localhost:{}", port), caps).await
+    WebDriver::new(format!("http://localhost:{port}"), caps).await
 }

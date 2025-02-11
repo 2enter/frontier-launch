91756ff4ed16b2635ee5afa67c49a636ea36db66
davilivob <david.iii.chiu@gmail.com>
2025-02-11 20:00:30 +0800

    [backend] create struct ApiResponse<T>

--- backend/model/src/util.rs
+++ backend/model/src/util.rs
@@ -6,3 +6,26 @@ use typeshare::typeshare;
 pub struct IdOnly {
     pub id: String,
 }
+
+#[typeshare]
+#[derive(Serialize, Debug)]
+pub struct ApiResponse<T> {
+    data: Option<T>,
+    error: Option<String>,
+}
+
+impl<T> ApiResponse<T> {
+    pub fn new_success(data: T) -> Self {
+        Self {
+            data: Some(data),
+            error: None,
+        }
+    }
+
+    pub fn new_error(error: String) -> Self {
+        Self {
+            data: None,
+            error: Some(error),
+        }
+    }
+}

7b862cf572f332575bd4aecc7b6f6b7cad1453c9
davilivob <david.iii.chiu@gmail.com>
2025-02-11 16:43:52 +0800

    [backend] add new config param: WD_PORT

--- backend/web/src/config.rs
+++ backend/web/src/config.rs
@@ -1,6 +1,7 @@
 #[derive(Clone, Debug)]
 pub struct Config {
     pub database_url: String,
+    pub wd_port: u16,
     pub port: u16,
     pub host: String,
 }
@@ -8,14 +9,16 @@ pub struct Config {
 impl Config {
     pub fn init() -> Self {
         let database_url = dotenvy::var("DATABASE_URL").expect("DATABASE_URL must be set");
+        let wd_port = dotenvy::var("WD_PORT").unwrap_or("4000".to_string());
         let port = dotenvy::var("PORT").unwrap_or("3000".to_string());
         let host = dotenvy::var("HOST").unwrap_or("0.0.0.0".to_string());
 
-        println!("configuration initialized: {host}:{port}, db: {database_url}");
+        println!("configuration initialized: {host}:{port}, db: {database_url}, wd: {wd_port}");
 
         Self {
             database_url,
             port: port.parse().unwrap_or(3000),
+            wd_port: wd_port.parse().unwrap_or(4000),
             host,
         }
     }

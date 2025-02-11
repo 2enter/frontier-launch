77a0d5378964fee9c457b069fcdb2ac23b3be25e
davilivob <david.iii.chiu@gmail.com>
2025-02-11 17:36:02 +0800

    [frontend] export API_BASE_URL from @/url

--- frontend/src/lib/url.ts
+++ frontend/src/lib/url.ts
@@ -1,5 +1,4 @@
 import { dev } from '$app/environment';
-import path from 'path';
 
 const API_BASE_URL = dev ? 'http://localhost:3080' : '';
 
@@ -11,4 +10,4 @@ function apiUrl(path: string) {
 	return `${API_BASE_URL}/api${path}`;
 }
 
-export { storageUrl, apiUrl };
+export { storageUrl, apiUrl, API_BASE_URL };

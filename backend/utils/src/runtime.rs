acb97724389fc82af0542cde0c445034baee2bf6
davilivob <david.iii.chiu@gmail.com>
2025-02-12 13:52:35 +0800

    [backend] better args naming

--- backend/utils/src/runtime.rs
+++ backend/utils/src/runtime.rs
@@ -1,9 +1,9 @@
 use async_std::task::sleep;
 use std::time::Duration;
 
-pub async fn rand_sleep(max: u32) {
+pub async fn rand_sleep(max_millis: u32) {
     sleep(Duration::from_millis(
-        (rand::random::<f32>() * max as f32) as u64,
+        (rand::random::<f32>() * max_millis as f32) as u64,
     ))
     .await;
 }

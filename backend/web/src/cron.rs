b5e5e12ed377927904c78de89ae8f7a4c77ad26b
davilivob <david.iii.chiu@gmail.com>
2025-02-12 13:53:16 +0800

    [backend] add tracing log & random sleep in cron tasks

--- backend/web/src/cron.rs
+++ backend/web/src/cron.rs
@@ -6,16 +6,18 @@ use model::cargo::Cargo;
 use model::news::News;
 use model::ws_msg::*;
 use tokio_cron_scheduler::{Job, JobScheduler, JobSchedulerError};
+use tracing::info;
 use utils::db::db_backup;
+use utils::runtime::rand_sleep;
 
 pub async fn init(app_state: AppState) -> Result<(), JobSchedulerError> {
-    let sched = JobScheduler::new().await?;
-
-    let send_weather = Job::new_async("every 10 minutes", {
+    let send_weather = Job::new_async("every 5 minutes", {
         let app_state = app_state.clone();
         move |_, _| {
             let sender = app_state.ws_sender.clone();
             Box::pin(async move {
+                rand_sleep(30000).await;
+                info!("Sending weather");
                 let raining = is_raining().await.unwrap_or(false);
                 let msg = WSMsg::weather(raining);
                 ws_broadcast(msg, &sender);
@@ -28,8 +30,8 @@ pub async fn init(app_state: AppState) -> Result<(), JobSchedulerError> {
         move |_, _| {
             let sender = app_state.ws_sender.clone();
             let pool = app_state.pool.clone();
-            println!("{} Launching rocket", chrono::Local::now());
             Box::pin(async move {
+                info!("Launching rocket");
                 let amount = Cargo::launch(&pool).await;
                 let msg = WSMsg::launch(amount);
                 ws_broadcast(msg, &sender);
@@ -42,6 +44,7 @@ pub async fn init(app_state: AppState) -> Result<(), JobSchedulerError> {
         move |_, _| {
             let pool = app_state.pool.clone();
             Box::pin(async move {
+                info!("Delivering cargoes");
                 Cargo::deliver(&pool).await;
             })
         }
@@ -54,6 +57,8 @@ pub async fn init(app_state: AppState) -> Result<(), JobSchedulerError> {
             let pool = app_state.pool.clone();
             Box::pin(async move {
                 let webdriver = get_webdriver(app_state.config.wd_port).await;
+                rand_sleep(3000).await;
+                info!("Fetching remote news");
                 if let Ok(driver) = webdriver {
                     if News::fetch_remote(&pool, &driver).await.is_err() {
                         println!("Failed to fetch remote news");
@@ -71,12 +76,16 @@ pub async fn init(app_state: AppState) -> Result<(), JobSchedulerError> {
         move |_, _| {
             let pool = app_state.pool.clone();
             Box::pin(async move {
+                info!("Backing up database");
                 if let Err(error) = db_backup(&pool, vec!["news", "cargo"], "../db/backups").await {
                     println!("Failed to backup database: {error}");
                 }
             })
         }
     })?;
+
+    // create scheduler
+    let sched = JobScheduler::new().await?;
 
     // add jobs to scheduler
     sched.add(send_weather).await?;

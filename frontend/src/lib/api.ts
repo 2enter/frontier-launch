f44586553f89bec33b2ee3621581bd41c5da0671
davilivob <david.iii.chiu@gmail.com>
2025-02-12 15:21:17 +0800

    [frontend] update api fetcher

--- frontend/src/lib/api.ts
+++ frontend/src/lib/api.ts
@@ -1,7 +1,8 @@
+import axios from 'axios';
 import { dev } from '$app/environment';
 import { PUBLIC_API_BASE_URL } from '$env/static/public';
-import axios, { type AxiosRequestConfig } from 'axios';
-import type { ApiResponse, Cargo, CargoInput } from '@/types/model';
+import type { Cargo, CargoInput } from '@/types/model';
+import { Api } from '@2enter/web-kit/runtime';
 
 const API_BASE_URL = dev ? PUBLIC_API_BASE_URL : '';
 
@@ -9,32 +10,26 @@ function apiUrl(path: string) {
 	return `${API_BASE_URL}/api${path}`;
 }
 
-const fetcher = axios.create({
-	baseURL: API_BASE_URL
-});
-
-async function lazyFetch<T>(config: AxiosRequestConfig) {
-	return await fetcher<ApiResponse<T>>(config).then((res) => res.data);
-}
+const api = new Api(axios.create({ baseURL: API_BASE_URL }));
 
 async function getNews() {
-	return lazyFetch<string[]>({ url: '/api/news' });
+	return api.fetch<string[]>({ url: '/api/news' });
 }
 
 async function getSysTemp() {
-	return lazyFetch<number>({ url: '/api/sys-temp' });
+	return api.fetch<number>({ url: '/api/sys-temp' });
 }
 
 async function sendCargoMetadata(input: CargoInput) {
-	return lazyFetch<Cargo>({ url: '/api/cargo/metadata', method: 'post', data: input });
+	return api.fetch<Cargo>({ url: '/api/cargo/metadata', method: 'post', data: input });
 }
 
 async function sendCargoImage(input: FormData) {
-	return lazyFetch<string>({ url: '/api/cargo/image', method: 'post', data: input });
+	return api.fetch<string>({ url: '/api/cargo/image', method: 'post', data: input });
 }
 
 async function getCargoes() {
-	return await lazyFetch<string[]>({ url: '/api/cargo/metadata' });
+	return api.fetch<string[]>({ url: '/api/cargo/metadata' });
 }
 
 export { API_BASE_URL, apiUrl, getCargoes, getNews, getSysTemp, sendCargoMetadata, sendCargoImage };

19ad5d1b8e9ac7ca8152480f572d3339fae91b0b
davilivob <david.iii.chiu@gmail.com>
2025-02-12 14:03:08 +0800

    [backend] shorten arg name for rand_sleep()

--- backend/utils/src/runtime.rs
+++ backend/utils/src/runtime.rs
@@ -1,9 +1,9 @@
 use async_std::task::sleep;
 use std::time::Duration;
 
-pub async fn rand_sleep(max_millis: u32) {
+pub async fn rand_sleep(max_ms: u32) {
     sleep(Duration::from_millis(
-        (rand::random::<f32>() * max_millis as f32) as u64,
+        (rand::random::<f32>() * max_ms as f32) as u64,
     ))
     .await;
 }

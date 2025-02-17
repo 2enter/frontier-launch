4844704c0cb3c6a69b1073ed881172a432788f1e
davilivob <david.iii.chiu@gmail.com>
2025-02-17 14:26:16 +0800

    [backend] feature: repeat ws message after receiving population data

--- backend/src/handlers/ws.rs
+++ backend/src/handlers/ws.rs
@@ -1,12 +1,25 @@
 use crate::state::AppState;
-use axum::extract::ws::WebSocket;
+use axum::extract::ws::{Message, WebSocket};
 use axum::extract::{State, WebSocketUpgrade};
 use axum::response::Response;
 use futures::{SinkExt, StreamExt};
+use model::ws_msg::WSMsg;
+use serde::Deserialize;
 use tokio::sync::broadcast::Sender;
 
 pub async fn ws_handler(ws: WebSocketUpgrade, State(app_state): State<AppState>) -> Response {
     ws.on_upgrade(move |socket| handle_socket(socket, app_state))
+}
+
+#[derive(Deserialize)]
+struct PopulationWsData {
+    r#type: String,
+    amount: u32,
+}
+
+#[derive(Deserialize)]
+struct PopulationWsMsg {
+    data: PopulationWsData,
 }
 
 async fn handle_socket(socket: WebSocket, state: AppState) {
@@ -22,8 +35,13 @@ async fn handle_socket(socket: WebSocket, state: AppState) {
         }
     });
 
-    while let Some(Ok(_)) = receiver.next().await {
-        // sender.send(msg).await.unwrap();
+    while let Some(Ok(msg)) = receiver.next().await {
+        tracing::info!("received websocket message: {msg:?}");
+        if let Message::Text(text) = msg {
+            if let Ok(value) = serde_json::from_str::<PopulationWsMsg>(text.to_string().as_str()) {
+                ws_broadcast(WSMsg::population(value.data.amount), &state.ws_sender);
+            }
+        }
     }
 }
 

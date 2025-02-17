b1f30129b32b7959d60c3a8e7325234eca864788
davilivob <david.iii.chiu@gmail.com>
2025-02-17 20:53:30 +0800

    [frontend] use @2enter/web-kit/tw-plugin

--- frontend/tailwind.config.ts
+++ frontend/tailwind.config.ts
@@ -1,14 +1,13 @@
 import type { Config } from 'tailwindcss';
-
+import { mobileLock, utils } from '@2enter/web-kit/tw-plugin';
+import daisyui from 'daisyui';
 export default {
 	content: [
 		'./src/**/*.{html,js,svelte,ts}',
 		'./node_modules/@2enter/web-kit/dist/components/*.svelte'
 	],
-
 	theme: {
 		extend: {}
 	},
-
-	plugins: [require('daisyui')]
+	plugins: [daisyui, utils, mobileLock]
 } satisfies Config;

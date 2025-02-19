978154b4d34474a1eab14bcdd9df460a2f0acc19
davilivob <david.iii.chiu@gmail.com>
2025-02-19 13:17:53 +0800

    [frontend] update vite http proxy config to read ../.env

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,12 +1,21 @@
 import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
+import dotenv from 'dotenv';
+
+dotenv.config({ path: '../.env' });
+
+const { BACKEND_PORT, BACKEND_HOST } = process.env;
+
+const API_BASE_URL = `http://${BACKEND_HOST ?? 'localhost'}:${BACKEND_PORT ?? 3000}`;
+
+console.log('API base url:', API_BASE_URL);
 
 export default defineConfig({
 	plugins: [sveltekit()],
 	server: {
 		proxy: {
 			'/api': {
-				target: 'http://localhost:3000',
+				target: API_BASE_URL,
 				changeOrigin: true
 			}
 		},

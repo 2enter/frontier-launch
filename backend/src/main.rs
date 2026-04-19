42d26433a0e53f42d3d0b4cd08218b829ebd6bf9
davilivob <david.iii.chiu@gmail.com>
2026-04-19 20:47:21 +0800

    [web] fix: typo

--- backend/src/main.rs
+++ backend/src/main.rs
@@ -35,7 +35,6 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
     let app = get_routes(app_state.clone());
 
     cron::init(app_state).await?;
-    cron::init(app_state).await?;
 
     let listener = tokio::net::TcpListener::bind("0.0.0.0:3000").await?;
     axum::serve(listener, app).await?;
